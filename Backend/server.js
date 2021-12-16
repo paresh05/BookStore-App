7d6ba3196cf700ca8e0676482e343b2236791713
paresh05 <pareshpraveen.surathkal@gmail.com>
2021-12-16 10:03:36 +0530

    Added: CRUD operations for Order details

--- Backend/server.js
+++ Backend/server.js
@@ -26,6 +26,7 @@ require("./app/routes/user.routes.js")(app);
 require("./app/routes/books.routes.js")(app);
 require("./app/routes/cart.routes.js")(app);
 require("./app/routes/customer.routes.js")(app);
+require("./app/routes/order.routes.js")(app);
 // listen for requests
 module.exports = app.listen(process.env.PORT, () => {
   console.log("Server is listening on port " + process.env.PORT);
